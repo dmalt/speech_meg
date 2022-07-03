c807ac9de7c5550a80ca2843f211e8b63f61a9e5
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-07-03 23:35:50 +0300

    add utils

--- rawdata/code/preproc/utils.py
+++ rawdata/code/preproc/utils.py
@@ -2,10 +2,14 @@ from __future__ import annotations
 
 from os import PathLike
 from pathlib import Path
+from typing import Optional
+
+from mne import Annotations, read_annotations  # type: ignore
+from mne.io import Raw, read_raw_fif  # type: ignore
 
 
-def read_bads(bads_path: PathLike) -> list[str]:
-    if not Path(bads_path).exists():
+def read_bads(bads_path: Optional[PathLike]) -> list[str]:
+    if bads_path is None or not Path(bads_path).exists():
         return []
     with open(bads_path, "r") as f:
         bads = f.readline().split("\t")
@@ -14,15 +18,29 @@ def read_bads(bads_path: PathLike) -> list[str]:
     return bads
 
 
-def write_bads(bads_path, bads):
-    with open(bads_path, "w") as f:
+def write_bad_channels(path: PathLike, bads: list[str]) -> None:
+    with open(path, "w") as f:
         f.write("\t".join(bads))
 
 
-def inspect_raw(raw, lowpass=100, highpass=None, n_channels=50):
+def write_annotations(path: PathLike, annotations: Annotations) -> None:
+    annotations.save(str(path), overwrite=True)
+
+
+def annotate_raw_manually(raw, lowpass=100, highpass=None, n_channels=50):
     """
     Manually mark bad channels and segments in gui signal viewer
     Filter chpi and line noise from data copy for inspection
     """
     raw.plot(block=True, lowpass=lowpass, highpass=highpass, n_channels=n_channels)
     return raw.info["bads"], raw.annotations
+
+
+def prepare_annotated_raw(raw_path: PathLike, bads_path: PathLike, annots_path: PathLike) -> Raw:
+    bads_path, annots_path = Path(bads_path), Path(annots_path)
+    raw_check = read_raw_fif(raw_path, preload=True)
+    raw_check.info["bads"] = read_bads(bads_path)
+    annotations = read_annotations(annots_path) if annots_path.exists() else None
+    if annotations is not None:
+        raw_check.set_annotations(annotations)
+    return raw_check
