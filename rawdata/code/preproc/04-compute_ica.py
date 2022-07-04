fa88f88a3281083e15214bb2e0c088a09e0a9a1a
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-07-04 15:18:05 +0300

    implement script 04 with hydra; add config for 03

--- rawdata/code/preproc/04-compute_ica.py
+++ rawdata/code/preproc/04-compute_ica.py
@@ -1,42 +1,47 @@
+#!/usr/bin/env python
+"""Compute ICA solution for raw data"""
+import logging
+import os
+
+import hydra
 from mne import Report  # type: ignore
-from mne import read_annotations  # type: ignore
-from mne.io import read_raw_fif  # type: ignore
+from mne.io import Raw  # type: ignore
 from mne.preprocessing import ICA  # type: ignore
-from speech import config as cfg  # type: ignore
+from utils import prepare_annotated_raw
+
+logger = logging.getLogger(__name__)
 
 
-def generate_report(raw, ica, report_savepath):
+def generate_report(ica: ICA) -> Report:
+    logger.info("Generating report...")
     report = Report(verbose=False)
-
     fig_topo = ica.plot_components(picks=range(ica.n_components_), show=False)
     report.add_figs_to_section(fig_topo, section="ICA", captions="Timeseries")
-    report.save(report_savepath, overwrite=True, open_browser=False)
+    return report
 
 
-def compute_ica(fif_path, ica_sol_path, annot_path):
-    raw = read_raw_fif(fif_path, preload=True)
-    annotations = read_annotations(annot_path) if annot_path.exists() else None
-    if annotations is not None:
-        raw.set_annotations(annotations)
-    raw.filter(l_freq=1, h_freq=None)
-    ica = ICA(cfg.ica_config["n_components"], random_state=cfg.ica_config["random_state"])
-    ica.fit(
-        raw,
-        picks="data",
-        decim=cfg.ica_config["decim"],
-        reject_by_annotation=cfg.ica_config["annot_rej"],
-    )
-    ica.save(ica_sol_path, overwrite=True)
+def compute_ica(raw: Raw, init_cfg, fit_cfg) -> ICA:
+    logger.info("Computing ICA...")
+    ica = ICA(init_cfg.n_components, random_state=init_cfg.random_state)
+    ica.fit(raw, picks="data", decim=fit_cfg.decim, reject_by_annotation=fit_cfg.annot_rej)
+    logger.info(f"Fitted {ica.n_components_} ICA components")
+    return ica
 
-    report_path = ica_sol_path.with_suffix(".html")
-    generate_report(raw, ica, report_path)
+
+@hydra.main(config_path="../configs/", config_name="04-apply_maxfilt")
+def main(cfg):
+    logger.info(f"Starting new session for {__file__}")
+    logger.info(f"Current working directory is {os.getcwd()}")
+
+    raw = prepare_annotated_raw(cfg.maxfilt.maxfiltered_path, None, cfg.annots.annotations_path)
+    raw.filter(l_freq=cfg.filt.l_freq, h_freq=cfg.filt.h_freq)
+
+    ica = compute_ica(raw, cfg.ica_init_cfg, cfg.ica_fit_cfg)
+    ica.save(cfg.ica_sol_path, overwrite=True)
+
+    report = generate_report(ica)
+    report.save(cfg.ica_report_path, overwrite=True, open_browser=False)
 
 
 if __name__ == "__main__":
-    # raw = cfg.cropped_path
-    raw = cfg.maxfilt_path
-    # output
-    ica_sol = cfg.ica_sol_path
-    annot = cfg.postmaxfilt_annotations_path
-
-    compute_ica(raw, ica_sol, annot)
+    main()
