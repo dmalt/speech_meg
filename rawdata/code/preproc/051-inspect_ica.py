aef44d08ee86c271a8339ce9a8beb1ed0a6a89de
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-07-05 23:10:08 +0300

    show annotation from file during ica inspection

--- rawdata/code/preproc/051-inspect_ica.py
+++ rawdata/code/preproc/051-inspect_ica.py
@@ -3,10 +3,8 @@
 import logging
 
 import hydra
-from mne.io import read_raw_fif  # type: ignore
 from mne.preprocessing import read_ica  # type: ignore
-
-from utils import prepare_script, read_ica_bads, write_ica_bads
+from utils import prepare_annotated_raw, prepare_script, read_ica_bads, write_ica_bads
 
 logger = logging.getLogger(__file__)
 
@@ -15,12 +13,15 @@ logger = logging.getLogger(__file__)
 def main(cfg):
     prepare_script(logger, script_name=__file__)
 
-    raw = read_raw_fif(cfg.input.raw, preload=True)
+    logger.info("Loading and filtering raw data")
+    raw = prepare_annotated_raw(cfg.input.raw, bads_path=None, annots_path=cfg.input.annots)
     raw.filter(l_freq=cfg.filt.l_freq, h_freq=cfg.filt.h_freq)
 
+    logger.info("Loading ICA solution and setting bad components")
     ica = read_ica(cfg.input.ica)
     ica.exclude = read_ica_bads(cfg.output.bad_ics)
     logger.info(f"ICs marked as bad: {ica.exclude}")
+
     ica.plot_sources(raw, block=True)
     write_ica_bads(cfg.output.bad_ics, ica)
 
