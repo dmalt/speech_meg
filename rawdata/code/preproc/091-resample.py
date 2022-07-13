51fe33ad3584a4e9ee732f96667ab7750520281e
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-07-14 00:53:35 +0300

    implement schema for 091

--- rawdata/code/preproc/091-resample.py
+++ rawdata/code/preproc/091-resample.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python
 import logging
+from dataclasses import dataclass
 
 import hydra
+from hydra.core.config_store import ConfigStore
 from mne.io import read_raw_fif  # type: ignore
-
-from utils import prepare_script
+from utils import BaseConfig, prepare_script
 
 logger = logging.getLogger(__file__)
 
 
+@dataclass
+class Input:
+    raw: str
+
+
+@dataclass
+class Output:
+    raw: str
+
+
+@dataclass
+class Config(BaseConfig):
+    input: Input
+    output: Output
+    sfreq: float
+
+
+cs = ConfigStore.instance()
+cs.store(name="schema", node=Config)
+
+
 @hydra.main(config_path="../configs/", config_name="091-resample")
-def main(cfg):
+def main(cfg: Config):
     prepare_script(logger, __file__)
 
     logger.info(f"Reading data from {cfg.input.raw}")
