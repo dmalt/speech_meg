50d935f22c2c8ac979657f5acbde564ecdef11b3
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-07-14 01:28:29 +0300

    remove obsolete code, refactor

--- rawdata/code/preproc/utils.py
+++ rawdata/code/preproc/utils.py
@@ -2,18 +2,27 @@ from __future__ import annotations
 
 import logging
 import os
-from os import PathLike, fspath
-from pathlib import Path
-from typing import Optional
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Any
 
-from mne import Annotations, read_annotations  # type: ignore
-from mne.io import Raw, read_raw_fif  # type: ignore
-from mne.preprocessing import ICA  # type: ignore
+import mne  # type: ignore
 
 
-def read_bads(bads_path: Optional[PathLike]) -> list[str]:
-    if bads_path is None or not Path(bads_path).exists():
-        return []
+class AnnotMode(Enum):
+    EDIT = auto()
+    NEW = auto()
+
+
+@dataclass
+class BaseConfig:
+    paths: Any
+    subj_id: str
+    task: str
+    deriv_paths: Any
+
+
+def read_bad_channels(bads_path: str) -> list[str]:
     with open(bads_path, "r") as f:
         bads = f.readline().split("\t")
     if bads == [""]:
@@ -21,72 +30,36 @@ def read_bads(bads_path: Optional[PathLike]) -> list[str]:
     return bads
 
 
-def write_bad_channels(path: PathLike, bads: list[str]) -> None:
-    with open(path, "w") as f:
+def write_bad_channels(savepath: str, bads: list[str]) -> None:
+    with open(savepath, "w") as f:
         f.write("\t".join(bads))
 
 
-def write_annotations(path: PathLike, annotations: Annotations) -> None:
-    annotations.save(fspath(path), overwrite=True)
+def read_ica_bads(ica_bads_path: str) -> list[int]:
+    return [int(c) for c in read_bad_channels(ica_bads_path)]
 
 
-def annotate_raw_manually(raw, lowpass=100, highpass=None, n_channels=50):
+def write_ica_bads(ica_bads_path: str, ica: mne.preprocessing.ICA) -> None:
+    write_bad_channels(ica_bads_path, [str(ic) for ic in ica.exclude])
+
+
+def update_annotations(raw: mne.io.Raw, annotations: mne.Annotations, overwrite=False) -> None:
     """
-    Manually mark bad channels and segments in gui signal viewer
-    Filter chpi and line noise from data copy for inspection
+    Set new annotations first and then add existing
+
+    This way we avoid conflict between annotations orig_time for `raw` and
+    `annotations` when added annotations are not coming from the same raw object,
+    i.e. generated programmatically
+
+    See also
+    --------
+    mne.Annotations: orig_time behaviour in more detail
+
     """
-    raw.plot(block=True, lowpass=lowpass, highpass=highpass, n_channels=n_channels)
-    return raw.info["bads"], raw.annotations
-
-
-def prepare_annotated_raw(
-    raw_path: PathLike, bads_path: Optional[PathLike], annots_path: Optional[PathLike]
-) -> Raw:
-    """Read raw data, and set bad channels and annotations loaded from files"""
-    raw_path = Path(raw_path)
-    raw = read_raw_fif(raw_path, preload=True)
-    raw.info["bads"] = read_bads(bads_path)
-
-    if annots_path is not None and Path(annots_path).exists():
-        annotations = read_annotations(Path(annots_path))
-        update_annotations(raw, annotations, overwrite=True)
-    return raw
-
-
-def update_annotations(raw: Raw, annotations: Annotations, overwrite=False) -> None:
-    # need to set new annotations first and then add existing. this way there's no conflict
-    # between their orig_time; see docs for mne.Annotations for more detail on orig_time
-    prev_annot = raw.annotations
+    prev_annot = raw.annotations if hasattr(raw, "annotations") else None
     raw.set_annotations(annotations)
     if prev_annot is not None and not overwrite:
         raw.set_annotations(raw.annotations + prev_annot)
-
-
-def annotation_pipeline(logger, cfg):
-    logger.info(f"Starting new session for {__file__}")
-    logger.info(f"Current working directory is {os.getcwd()}")
-
-    raw = prepare_annotated_raw(cfg.input.raw, cfg.output.bad_ch, cfg.output.annots)
-    bads, annotations = annotate_raw_manually(raw)
-    write_bad_channels(cfg.output.bad_ch, bads)
-    write_annotations(cfg.output.annots, annotations)
-
-    logger.info(f"Channels marked as bad: {bads}")
-    logger.info(f"Annotations: {annotations}")
-
-
-def read_ica_bads(ica_bads_path: PathLike) -> list[int]:
-    if not Path(ica_bads_path).exists():
-        return []
-    with open(ica_bads_path, "r") as f:
-        line = f.readline()
-        bads = [int(b) for b in line.split("\t")] if line else []
-    return bads
-
-
-def write_ica_bads(ica_bads_path: PathLike, ica: ICA) -> None:
-    with open(ica_bads_path, "w") as f:
-        f.write("\t".join([str(ic) for ic in ica.exclude]))
 
 
 def prepare_script(logger: logging.Logger, script_name: str) -> None:
