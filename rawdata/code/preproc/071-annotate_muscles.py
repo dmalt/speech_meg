3fc3f59fdf02ddf8557377fbebb695c18218c0e0
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-07-14 00:34:50 +0300

    refactor & implement schema for 071

--- rawdata/code/preproc/071-annotate_muscles.py
+++ rawdata/code/preproc/071-annotate_muscles.py
@@ -1,51 +1,71 @@
 #!/usr/bin/env python
 """Automatically mark muscle segments and edit them manually"""
-from __future__ import annotations
-
 import logging
-import sys
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Tuple
+from typing import List
 
 import hydra
 import matplotlib  # type: ignore
-from mne import read_annotations  # type: ignore
-from mne.io import read_raw_fif  # type: ignore
-from mne.preprocessing import annotate_muscle_zscore  # type: ignore
+import mne  # type: ignore
+import mne.preprocessing as mne_preproc  # type: ignore
+from hydra.core.config_store import ConfigStore
 from omegaconf import OmegaConf
-
-from utils import prepare_script, update_annotations
+from utils import AnnotMode, BaseConfig, prepare_script
 
 matplotlib.use("TkAgg")
 
 logger = logging.getLogger(__file__)
 
-Band = Tuple[float, float]
+
+@dataclass
+class Input:
+    raw: str
+
+
+@dataclass
+class Output:
+    annots: str
+
+
+@dataclass
+class AnnotateMuscleParams:
+    threshold: float
+    filter_freq: List[int]
+    min_length_good: float
+    ch_type: str
+
+
+@dataclass
+class Config(BaseConfig):
+    input: Input
+    output: Output
+    mode: AnnotMode
+    annotate_muscle_params: AnnotateMuscleParams
+
+
+cs = ConfigStore.instance()
+cs.store(name="schema", node=Config)
 
 
 @hydra.main(config_path="../configs/", config_name="071-annotate_muscles")
-def main(cfg):
+def main(cfg: Config):
     prepare_script(logger, script_name=__file__)
 
-    raw = read_raw_fif(cfg.input.raw, preload=True)
-    if not Path(cfg.output.annots).exists() or cfg.mode == "new":
+    raw = mne.io.read_raw_fif(cfg.input.raw, preload=True)
+    if not Path(cfg.output.annots).exists() or cfg.mode == AnnotMode.NEW:
         logger.info("Creating new muscle annotations")
-        params = OmegaConf.to_container(cfg.annotate_muscle_zscore_params)
-        muscle_annots, _ = annotate_muscle_zscore(raw, **params)  # pyright: ignore
+        params = OmegaConf.to_container(cfg.annotate_muscle_params)
+        muscle_annots, _ = mne_preproc.annotate_muscle_zscore(raw, **params)  # pyright: ignore
         raw.set_annotations(muscle_annots)
-        prev_annots = read_annotations(cfg.input.annots)
-        update_annotations(raw, prev_annots)
-        logger.info(f"Auto muscle annotations + preexisting ones: {prev_annots}")
-    elif cfg.mode == "edit":
+        logger.info(f"Auto muscle annotations: {muscle_annots}")
+    else:
         logger.info(f"Editing existing annotations at {cfg.output.annots}")
-        edited_muscle_annots = read_annotations(cfg.output.annots)
+        edited_muscle_annots = mne.read_annotations(cfg.output.annots)
         logger.info(f"Loaded annotations: {edited_muscle_annots}")
         raw.set_annotations(edited_muscle_annots)
-    else:
-        logger.error(f"Bad mode type {cfg.mode=}. Should be `new` or `edit`")
-        sys.exit(1)
     raw.plot(block=True)
-    logger.info(f"Final annotations: {annotations}")
+    logger.info(f"Final annotations: {raw.annotations}")
     raw.annotations.save(cfg.output.annots, overwrite=True)
 
 
