0b1aedf25b9bbdaee904d86866b46a8afdb13264
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-07-14 01:07:08 +0300

    fix, refactor, implement schema

--- rawdata/code/preproc/101-merge_annotations.py
+++ rawdata/code/preproc/101-merge_annotations.py
@@ -2,37 +2,60 @@
 
 import logging
 import sys
+from dataclasses import dataclass
 from functools import reduce
+from pathlib import Path
+from typing import List
 
 import hydra
 import matplotlib  # type: ignore
-from mne import read_annotations  # type: ignore
-from mne.io.fiff.raw import read_raw_fif  # type: ignore
-from utils import prepare_script, write_annotations
+import mne  # type: ignore
+from hydra.core.config_store import ConfigStore
+from mne import read_annotations
+from utils import AnnotMode, BaseConfig, prepare_script
 
 matplotlib.use("TkAgg")
 
 logger = logging.getLogger(__file__)
 
 
+@dataclass
+class Input:
+    raw: str
+    annots_list: List[str]
+
+
+@dataclass
+class Output:
+    annots: str
+
+
+@dataclass
+class Config(BaseConfig):
+    input: Input
+    output: Output
+    mode: AnnotMode
+
+
+cs = ConfigStore.instance()
+cs.store(name="schema", node=Config)
+
+
 @hydra.main(config_path="../configs/", config_name="101-merge_annotations")
-def main(cfg):
+def main(cfg: Config):
     prepare_script(logger, script_name=__file__)
 
-    if cfg.mode == "new":
+    if not Path(cfg.output.annots).exists() or cfg.mode == AnnotMode.NEW:
         annots = reduce(lambda x, y: x + y, (read_annotations(a) for a in cfg.input.annots_list))
-    elif cfg.mode == "edit":
-        annots = read_annotations(cfg.output.annots)
     else:
-        logger.error(f"Bad mode type {cfg.mode=}. Should be `new` or `edit`")
-        sys.exit(1)
+        annots = mne.read_annotations(cfg.output.annots)
 
-    raw = read_raw_fif(cfg.input.raw)
+    raw = mne.io.read_raw_fif(cfg.input.raw)
     raw.set_annotations(annots)
     raw.plot(block=True)
 
     logger.info(f"Annotations: {annots}")
-    write_annotations(cfg.output.annots, annots)
+    annots.save(cfg.output.annots, overwrite=True)
 
 
 if __name__ == "__main__":
