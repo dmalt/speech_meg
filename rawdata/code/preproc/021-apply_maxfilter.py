296383e369c65d5067a27ce7f4c102af825cfd4a
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-07-13 20:02:43 +0300

    add schema to 021

--- rawdata/code/preproc/021-apply_maxfilter.py
+++ rawdata/code/preproc/021-apply_maxfilter.py
@@ -11,21 +11,55 @@ for emptyroom data
 
 """
 import logging
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Union
 
 import hydra
+from hydra.core.config_store import ConfigStore
 from mne.channels import fix_mag_coil_types  # type: ignore
 from mne.chpi import filter_chpi  # type: ignore
+from mne.io.fiff.raw import read_raw_fif  # type: ignore
 from mne.preprocessing import maxwell_filter  # type: ignore
-from utils import prepare_annotated_raw, prepare_script
+from utils import BaseConfig, prepare_script, read_bad_channels
 
 logger = logging.getLogger(__file__)
 
 
+@dataclass
+class Input:
+    raw: str
+    ct: str
+    cal: str
+    bad_ch: str
+    annots: str
+
+
+@dataclass
+class Output:
+    maxfilt_raw: str
+
+
+@dataclass
+class Config(BaseConfig):
+    input: Input
+    output: Output
+    t_window: Any = "auto"  # float or str; Union types are not supported yet
+
+
+cs = ConfigStore.instance()
+cs.store(name="schema", node=Config)
+
+
 @hydra.main(config_path="../configs/", config_name="021-apply_maxfilter")
-def main(cfg):
+def main(cfg: Config):
     prepare_script(logger, script_name=__file__)
 
-    raw = prepare_annotated_raw(cfg.input.raw, cfg.input.bad_ch, cfg.input.annots)
+    raw = read_raw_fif(cfg.input.raw, preload=True)
+    if Path(cfg.input.bad_ch).exists():
+        raw.info["bads"] = read_bad_channels(cfg.input.bad_ch)
+    else:
+        logger.warning(f"Missing bad channels file at {cfg.input.bad_ch}")
     fix_mag_coil_types(raw.info)
     filter_chpi(raw, t_window=cfg.t_window)
     raw = maxwell_filter(
