df4beb18e4c7970995c3de2cd78edf7abbe10bb7
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-07-13 19:31:56 +0300

    add schema

--- rawdata/code/preproc/031-annotate_postmaxfilt.py
+++ rawdata/code/preproc/031-annotate_postmaxfilt.py
@@ -1,20 +1,60 @@
 #!/usr/bin/env python
 """Manually mark bad segments after maxfilter for ICA"""
 import logging
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
 
 import hydra
 import matplotlib  # type: ignore
-
-from utils import annotation_pipeline
+from hydra.core.config_store import ConfigStore
+from mne.annotations import read_annotations  # type: ignore
+from mne.io.fiff.raw import read_raw_fif  # type: ignore
+from utils import AnnotMode, BaseConfig, prepare_script
 
 matplotlib.use("TkAgg")
-
 logger = logging.getLogger(__file__)
 
 
+@dataclass
+class Input:
+    raw: str
+    annots: str
+
+
+@dataclass
+class Output:
+    annots: str
+
+
+@dataclass
+class Config(BaseConfig):
+    input: Input
+    output: Output
+    mode: AnnotMode = AnnotMode.EDIT
+    lowpass: Optional[float] = 100
+    highpass: Optional[float] = None
+    n_channels: int = 50
+
+
+cs = ConfigStore.instance()
+cs.store(name="schema", node=Config)
+
+
 @hydra.main(config_path="../configs/", config_name="031-annotate_postmaxfilt")
-def main(cfg):
-    annotation_pipeline(logger, cfg)
+def main(cfg: Config):
+    prepare_script(logger, script_name=__file__)
+
+    raw = read_raw_fif(cfg.input.raw)
+    if Path(cfg.input.annots).exists() and cfg.mode == AnnotMode.NEW:
+        raw.set_annotations(read_annotations(cfg.input.annots))
+    if Path(cfg.output.annots).exists() and cfg.mode == AnnotMode.EDIT:
+        raw.set_annotations(read_annotations(cfg.output.annots))
+
+    raw.plot(block=True, lowpass=cfg.lowpass, highpass=cfg.highpass, n_channels=cfg.n_channels)
+
+    logger.info(f"Annotations: {raw.annotations}")
+    raw.annotations.save(cfg.output.annots, overwrite=True)
 
 
 if __name__ == "__main__":
