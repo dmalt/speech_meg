157921e26a0f3f56a0b86a241d443c92b1ced594
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-07-15 12:09:16 +0300

    add interface to the dataset
    - add code to load the data
    - add setup.py
    - add py.typed marker
    - ignore egg

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="speech_meg",
+    version="0.0.1",
+    author="dmitrii altukhov",
+    author_email="dm.altukhov@ya.ru",
+    py_modules=["speech_meg"],
+)
